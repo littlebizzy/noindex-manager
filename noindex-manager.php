9bad2f3833f0d317524c663c4e5994cdac4bff84
Jesse Nickles <jesse@littlebizzy.com>
2023-01-28 16:24:23 +0700

    Update noindex-manager.php

--- noindex-manager.php
+++ noindex-manager.php
@@ -25,7 +25,7 @@ add_filter(
 );
 
 // noindex junk bbpress pages
-if ( !function_exists( 'topic_add_noindex' ) ):
+if ( !function_exists( 'noindex_bbpress_thin_pages' ) ):
 function topic_add_noindex() {
 	if (bbp_is_single_user()) { // bbpress user profiles
     	wp_no_robots();
@@ -41,6 +41,6 @@ function topic_add_noindex() {
 	}	
 }
 endif;
-add_action( 'wp_head', 'topic_add_noindex' );
+add_action( 'wp_head', 'noindex_bbpress_thin_pages' );
 
 // https://bbpress.org/forums/topic/add-noindex-to-some-forum-pages/
