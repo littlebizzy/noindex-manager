d43a3782536fe0ee39cc0f08a3b227aef4e19ad4
Jesse Nickles <jesse@littlebizzy.com>
2023-02-04 02:18:43 +0700

    Update noindex-manager.php

--- noindex-manager.php
+++ noindex-manager.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name:  Noindex Manager
 Plugin URI: https://www.littlebizzy.com/plugins/noindex-manager
-Description: Noindex thin WordPress content
-Version: 1.1.0
+Description: Noindex junk WordPress content
+Version: 1.0.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -24,39 +24,23 @@ add_filter(
 	}
 );
 
-// noindex woocommerce thin content
-if ( !function_exists( 'noindex_woocommerce_thin_content' ) ):
-function noindex_woocommerce_thin_content() {
-	if ( is_product_taxonomy() ) { // woocommerce product attribute archives
-    	wp_no_robots();
-  	}
-	if ( is_tax() && function_exists( 'taxonomy_is_product_attribute' ) ) { // individual product attribute page
-    	wp_no_robots();
-  	}
-}
-endif;
-add_action( 'wp_head', 'noindex_woocommerce_thin_content' );
-
 // noindex bbpress thin content
 if ( !function_exists( 'noindex_bbpress_thin_content' ) ):
 function noindex_bbpress_thin_content() {
-	if ( bbp_is_single_user() ) { // bbpress user profiles
+	if ( function_exists( 'bbp_is_single_user' ) && bbp_is_single_user() ) { // bbpress user profiles
     	wp_no_robots();
   	}	
-	if ( bbp_is_single_topic() && bbp_get_topic_reply_count()<=3) { // topics with few replies
+	if ( function_exists( 'bbp_is_single_topic' ) && bbp_is_single_topic() && bbp_get_topic_reply_count()<=3) { // topics with few replies
     	wp_no_robots();
 	}
-	if ( bbp_is_single_view() ) { // view links
+	if ( function_exists( 'bbp_is_single_view' ) && bbp_is_single_view() ) { // view links
     	wp_no_robots();
 	}	
-	if ( bbp_is_single_reply() ) { // reply links
+	if ( function_exists( 'bbp_is_single_reply' ) && bbp_is_single_reply() ) { // reply links
     	wp_no_robots();
 	}	
 }
 endif;
 add_action( 'wp_head', 'noindex_bbpress_thin_content' );
 
-
 // https://bbpress.org/forums/topic/add-noindex-to-some-forum-pages/
-// https://wordpress.stackexchange.com/questions/333424/check-if-page-is-a-woocommerce-attribute
-// https://stackoverflow.com/questions/72013417/check-the-woocommerce-product-attribute-on-archive-page-and-do-something
