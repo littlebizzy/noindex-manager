22d5c1ce4b14d34ecfbc1750f25ec3365b3dd1d7
Jesse Nickles <jesse@littlebizzy.com>
2023-02-04 14:27:19 +0700

    Update noindex-manager.php

--- noindex-manager.php
+++ noindex-manager.php
@@ -3,7 +3,7 @@
 Plugin Name:  Noindex Manager
 Plugin URI: https://www.littlebizzy.com/plugins/noindex-manager
 Description: Noindex junk WordPress content
-Version: 1.0.0
+Version: 1.1.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -29,18 +29,21 @@ if ( !function_exists( 'noindex_bbpress_thin_content' ) ):
 function noindex_bbpress_thin_content() {
 	if ( function_exists( 'bbp_is_single_user' ) && bbp_is_single_user() ) { // bbpress user profiles
     	wp_no_robots();
-  	}	
+  	}
 	if ( function_exists( 'bbp_is_single_topic' ) && bbp_is_single_topic() && bbp_get_topic_reply_count()<=3) { // topics with few replies
     	wp_no_robots();
 	}
 	if ( function_exists( 'bbp_is_single_view' ) && bbp_is_single_view() ) { // view links
     	wp_no_robots();
-	}	
+	}
 	if ( function_exists( 'bbp_is_single_reply' ) && bbp_is_single_reply() ) { // reply links
     	wp_no_robots();
-	}	
+	}
 }
 endif;
 add_action( 'wp_head', 'noindex_bbpress_thin_content' );
 
+
 // https://bbpress.org/forums/topic/add-noindex-to-some-forum-pages/
+// https://wordpress.stackexchange.com/questions/333424/check-if-page-is-a-woocommerce-attribute
+// https://stackoverflow.com/questions/72013417/check-the-woocommerce-product-attribute-on-archive-page-and-do-something
